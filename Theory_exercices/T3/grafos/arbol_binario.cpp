0b5f48eb6787944ce2bc2a89e5088962895bd776
Marco_Girela <marcogirelavida@gmail.com>
2023-11-10 22:57:58 +0100

    Continued with implementation of ADT binary-tree

--- Theory_exercices/T3/grafos/arbol_binario.cpp
+++ Theory_exercices/T3/grafos/arbol_binario.cpp
@@ -1,7 +1,9 @@
 /*
 
 */
-
+#include <iostream>
+#include <queue>
+using namespace std;
 template <class T>
 struct info_nodo{
     T et;
@@ -18,22 +20,23 @@ struct info_nodo{
         padre = 0;
         hijoizq = hijoder = 0;
     }
+
+
+    template<class T>
+    info_nodo<T> * getPadre (info_nodo<T> *n) {
+        return n->padre;
+    }
+
+    template<class T>
+    info_nodo<T> * getHijoIzq(info_nodo<T> *n) {
+        return n->hijoizq;
+    }
+
+    template<class T>
+    info_nodo<T> * getHijoDer(info_Nodo<T> *n) {
+        return n->hijoder;
+    }
 };
-
-template<class T>
-info_nodo<T> * getPadre (info_nodo<T> *n) {
-    return n->padre;
-}
-
-template<class T>
-info_nodo<T> * getHijoIzq(info_nodo<T> *n) {
-    return n->hijoizq;
-}
-
-template<class T>
-info_nodo<T> * getHijoDer(info_Nodo<T> *n) {
-    return n->hijoder;
-}
 
 template<class T>
 void Copiar (const info_nodo<T> *s, info_nodo<T> *&d) {
@@ -110,9 +113,87 @@ void PodarHijoIzquierdaGetSubtree(info_nodo<T> *n)
     n->hijoizq = nullptr;
     if (auv != nullptr)
     {
-        aux->padre=nullptr
+        aux->padre=nullptr;
     }
 
     return aux;
+}
+
+template<class T>
+void RecorridoPreOrden(info_nodo<T> * n)
+{
+    if (n != nullptr)
+    {
+        cout << n->etiqueta<<' ';
+        RecorridoPreorden (n->hijoizq);
+        RecorridoPreorden (n->hijoder);
+    } 
+}
+
+template<class T>
+void RecorridoInorden(info_nodo<T> * n)
+{
+    if (n != nullptr)
+    {
+        RecorridoInorden(n->hijoizq);
+        cout << n->etiqueta << ' ';
+        RecorridoInorden(n->hijoder);
+    }
+}
+
+template<class T>
+void RecorridoPostorden(info_nodo<T> * n)
+{
+    if (n != nullptr)
+    {
+        RecorridoInorden(n->hijoizq);
+        RecorridoInorden(n->hijoder);
+        cout << n->etiqueta << ' ';
+    }
+}
+// TRUCO: Postorden, el cout va al final, inorden en medio, preorden al principio
+
+// Ejercicio ejemplo que requiera recorrido por niveles: Calcular la suma de cada nivel
+// Una opción sería añadir un parámetro en infonodo que nos diga el nivel
+template<class T>
+void RecorridoNiveles (info_nodo<T> * n)
+{
+    queue<info_nodo<T>> micola;
+    micola.push(n);
+    while (!micola.empty())
+    {
+        info_nodo<T> * aux = micola.front();
+        micola.pop();
+        cout << aux->etiqueta << ' ';
+        if (aux->hijoizq != nullptr)
+            micola.push(aux->hijoizq);
+        
+        if (aux->hijoder != nullptr)
+            micola.push(aux->hijoder);
+    }
+
+    recorrido.pop();
+}
+
+template<class T>
+bool iguales(info_nodo<T>*n1, info_nodo<T> *n2) {
+    if (n1 == nullptr && n2 == nullptr) 
+        return true;
+
+    if (n1==nullptr || n2==nullptr)
+        return false;
     
+    if (n1->etiqueta != n2->etiqueta)
+        return false;
+    else
+        return iguales(n1->hijoizq, n2->hijoizq) && iguales(n1->hijoder, n2->hijoder);
+}
+
+template<class T>
+int cuenta_nodos(info_nodos<T> * n)
+{
+    if (n == nullptr)
+        return 0;
+    else
+        return numero_nodos(n->hijoizq) + numero_nodos(n->hijoder) +1;
 }
